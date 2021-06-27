5e6c58a6bba493339566ca653401fcc2d3ba9b07
Mickael <mickael.monsieur@gmail.com>
2021-06-27 18:46:59 +0200

    sscategories CRUD

--- U_Category.dfm
+++ U_Category.dfm
@@ -1,8 +1,8 @@
 object Category: TCategory
-  Left = 593
-  Top = 239
-  Width = 736
-  Height = 451
+  Left = 510
+  Top = 360
+  Width = 941
+  Height = 164
   Caption = 'Category'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -11,13 +11,84 @@ object Category: TCategory
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
-  object Label1: TLabel
-    Left = 320
-    Top = 176
-    Width = 42
-    Height = 13
-    Caption = 'A faire :-)'
+  object GroupBox1: TGroupBox
+    Left = 8
+    Top = 4
+    Width = 897
+    Height = 109
+    Caption = 'Modifier le nom de la cat'#233'gorie'
+    TabOrder = 0
+    object Categorie: TComboBox
+      Left = 13
+      Top = 32
+      Width = 265
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 0
+      OnChange = CategorieChange
+    end
+    object Button1: TButton
+      Left = 594
+      Top = 31
+      Width = 75
+      Height = 23
+      Caption = 'Modifier'
+      TabOrder = 1
+      Visible = False
+      OnClick = Button1Click
+    end
+    object SSCategorie: TComboBox
+      Left = 13
+      Top = 60
+      Width = 265
+      Height = 21
+      ItemHeight = 13
+      TabOrder = 2
+      OnChange = SSCategorieChange
+    end
+    object Button2: TButton
+      Left = 593
+      Top = 60
+      Width = 75
+      Height = 23
+      Caption = 'Modifier'
+      TabOrder = 3
+      OnClick = Button2Click
+    end
+    object CategorieName: TEdit
+      Left = 296
+      Top = 32
+      Width = 289
+      Height = 21
+      TabOrder = 4
+    end
+    object SSCategorieName: TEdit
+      Left = 296
+      Top = 60
+      Width = 289
+      Height = 21
+      TabOrder = 5
+    end
+    object Button3: TButton
+      Left = 674
+      Top = 60
+      Width = 75
+      Height = 23
+      Caption = 'Ajouter'
+      TabOrder = 6
+      OnClick = Button3Click
+    end
+    object Button4: TButton
+      Left = 754
+      Top = 60
+      Width = 75
+      Height = 23
+      Caption = 'Supprimer'
+      TabOrder = 7
+      OnClick = Button4Click
+    end
   end
 end
--- U_Category.pas
+++ U_Category.pas
@@ -4,11 +4,27 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls;
+  Dialogs, StdCtrls, MysqlComponent;
 
 type
   TCategory = class(TForm)
-    Label1: TLabel;
+    GroupBox1: TGroupBox;
+    Categorie: TComboBox;
+    Button1: TButton;
+    SSCategorie: TComboBox;
+    Button2: TButton;
+    CategorieName: TEdit;
+    SSCategorieName: TEdit;
+    Button3: TButton;
+    Button4: TButton;
+    procedure FormShow(Sender: TObject);
+    procedure CompleteCategories(Sender: TObject);
+    procedure CategorieChange(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure SSCategorieChange(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -20,6 +36,170 @@ var
 
 implementation
 
+uses U_Welcome;
+
 {$R *.dfm}
+
+function getCategoryName(id: string): string;
+var
+  Res: PMYSQL_RES;
+  Row: PMYSQL_ROW;
+begin
+
+  Res := welcome.sql.Query('SELECT nom FROM categories WHERE id=' + id + ' LIMIT 1;');
+  if (Res <> nil) then
+  begin
+    try
+      Row := welcome.sql.fetch_row(Res);
+      if (Row <> nil) then
+      begin
+        Result := Row[0];
+      end;
+    except;
+      welcome.sql.free_result(Res);
+    end;
+  end;
+
+end;
+
+function getSSCategoryName(id: string): string;
+var
+  Res: PMYSQL_RES;
+  Row: PMYSQL_ROW;
+begin
+
+  Res := welcome.sql.Query('SELECT nom FROM sscategories WHERE id=' + id + ' LIMIT 1;');
+  if (Res <> nil) then
+  begin
+    try
+      Row := welcome.sql.fetch_row(Res);
+      if (Row <> nil) then
+      begin
+        Result := Row[0];
+      end;
+    except;
+      welcome.sql.free_result(Res);
+    end;
+  end;
+
+end;
+
+procedure TCategory.FormShow(Sender: TObject);
+begin
+  CompleteCategories(Sender);
+end;
+
+procedure TCategory.CompleteCategories(Sender: TObject);
+var
+  Res: PMYSQL_RES;
+  Row: PMYSQL_ROW;
+  i: Integer;
+begin
+
+  with Categorie.Items do
+    for i := Count - 1 downto 0 do
+      Delete(i);
+
+  with SSCategorie.Items do
+    for i := Count - 1 downto 0 do
+      Delete(i);
+
+  Res := welcome.sql.Query('SELECT id, nom FROM categories;');
+  Row := welcome.sql.fetch_row(Res);
+
+  while Row <> nil do
+  begin
+    Categorie.AddItem('(' + Row[0] + ') ' + Row[1], TObject(StrToInt(Row[0])));
+    Row := welcome.sql.fetch_row(Res);
+  end;
+
+  Categorie.ItemIndex := 0;
+  CategorieChange(sender);
+
+  welcome.sql.free_result(Res);
+end;
+
+procedure TCategory.CategorieChange(Sender: TObject);
+var
+  Res: PMYSQL_RES;
+  Row: PMYSQL_ROW;
+  i: Integer;
+  RequestSQL, currentId: string;
+begin
+
+  currentId := IntToStr(integer(Categorie.items.objects[Categorie.itemindex]));
+  CategorieName.Text := getCategoryName(currentId);
+
+  with SSCategorie.Items do
+    for i := Count - 1 downto 0 do
+      Delete(i);
+
+  RequestSQL := 'SELECT id, nom FROM sscategories WHERE categorie=' + currentId + ';';
+  Res := welcome.sql.Query(RequestSQL);
+
+  Row := welcome.sql.fetch_row(Res);
+  while Row <> nil do
+  begin
+    SSCategorie.AddItem('(' + Row[0] + ') ' + Row[1], TObject(StrToInt(Row[0])));
+    Row := welcome.sql.fetch_row(Res);
+  end;
+
+  if (SSCategorie.Items.Count = 0) then begin
+    SSCategorie.AddItem('(0) Default', TObject(0));
+  end;
+
+  SSCategorie.ItemIndex := 0;
+
+  welcome.sql.free_result(Res);
+end;
+
+procedure TCategory.Button1Click(Sender: TObject);
+begin
+  if(CategorieName.Text <> '') then
+  begin
+    welcome.sql.Query('UPDATE categories SET nom=''' + CategorieName.Text + ''' WHERE id=' + IntToStr(integer(Categorie.items.objects[Categorie.itemindex])));
+    CompleteCategories(Sender);
+  end;
+end;
+
+procedure TCategory.Button2Click(Sender: TObject);
+var
+  i: Integer;
+begin
+  if(SSCategorieName.Text <> '') then
+  begin
+    welcome.sql.Query('UPDATE sscategories SET nom=''' + SSCategorieName.Text + ''' WHERE id=' + IntToStr(integer(SSCategorie.items.objects[SSCategorie.itemindex])));
+    i := SSCategorie.itemindex;
+    CategorieChange(Sender);
+    SSCategorie.itemindex := i;
+  end;
+end;
+
+procedure TCategory.SSCategorieChange(Sender: TObject);
+var
+  currentId: string;
+begin
+
+  currentId := IntToStr(integer(SSCategorie.items.objects[SSCategorie.itemindex]));
+  SSCategorieName.Text := getSSCategoryName(currentId);
+
+end;
+
+procedure TCategory.Button3Click(Sender: TObject);
+begin
+  if(SSCategorieName.Text <> '') then
+  begin
+    welcome.sql.Query('INSERT INTO sscategories SET categorie = '+ IntToStr(integer(Categorie.items.objects[Categorie.itemindex])) +', nom=''' + SSCategorieName.Text + ''';');
+    CategorieChange(Sender);
+    SSCategorie.itemindex := SSCategorie.Items.Count - 1;
+  end;
+end;
+
+procedure TCategory.Button4Click(Sender: TObject);
+begin
+  welcome.sql.Query('DELETE FROM sscategories WHERE id=' + IntToStr(integer(SSCategorie.items.objects[SSCategorie.itemindex])));
+  CategorieChange(Sender);
+  SSCategorieName.Text := '';
+end;
 
 end.
